1e7cab3a650f736349c4fd386dc38ebeb68ea5bb
AlexiWolf <alexiwolf@pm.me>
2022-05-07 20:05:38 -0500

    Ran cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,23 @@
 //! The main job of Input Helper is to provide a single, unified way to process input
-//! from any input framework. 
+//! from any input framework.
 //!
 //! # The Problem
 //!
-//! Most libraries that take user input do so in a library-specific way.  In order to 
+//! Most libraries that take user input do so in a library-specific way.  In order to
 //! use the library, you must write library-specific code.
 //!
-//! For example: You can't switch from Winit to SDL, or Gilrs to SDL, or any other 
-//! library without having to rewrite all your input code. This can either couple your 
+//! For example: You can't switch from Winit to SDL, or Gilrs to SDL, or any other
+//! library without having to rewrite all your input code. This can either couple your
 //! project to specific framework(s), or force you to waste time duplicating code.  In the
-//! case of using multiple input libraries, such as pairing Winit and Gilrs you must 
+//! case of using multiple input libraries, such as pairing Winit and Gilrs you must
 //! handle each framework separately.
 //!
 //! # The Solution
 //!
-//! Library-specific input events can be converted to [InputEvent]s, and sent through 
-//! Input Helper using [InputHelper::send()].  Then your application code can be written 
+//! Library-specific input events can be converted to [InputEvent]s, and sent through
+//! Input Helper using [InputHelper::send()].  Then your application code can be written
 //! against Input Helper.  If you ever need to switch your input system, then there's no
-//! need to rewrite your business logic.  This also allows you to process all input code 
+//! need to rewrite your business logic.  This also allows you to process all input code
 //! in a single place regardless of its source.
 //!
 //! Input Helper will provide default integration functions for common frameworks such as
@@ -26,22 +26,22 @@
 //! # Getting Started
 //!
 //! Create the [InputHelper] and send it some [InputEvent] events.
-//! 
+//!
 //! ```
 //! use input_helper::*;
 //!
 //! let input_helper = InputHelper::new();
-//! 
+//!
 //! input_helper.send(InputEvent::KeyDown("a"));
 //! input_helper.send(InputEvent::KeyUp("a"));
 //! ```
 //!
-//! Read events using an [InputReader].  You can request a new [InputReader] using 
+//! Read events using an [InputReader].  You can request a new [InputReader] using
 //! [InputHelper::reader()].
 //!
 //! ```
 //! # use input_helper::*;
-//! # 
+//! #
 //! # let input_helper = InputHelper::new();
 //! #
 //! let input_reader = input_helper.reader();
@@ -51,23 +51,23 @@
 //! }
 //! ```
 //!
-//! Query input state using an [InputMap].  An [InputMap] is created from an existing 
+//! Query input state using an [InputMap].  An [InputMap] is created from an existing
 //! [InputHelper] instance.
 //!
 //! ```
 //! # use input_helper::*;
-//! # 
+//! #
 //! # let input_helper = InputHelper::new();
 //! #
-//! let mut input_map = InputMap::new(&input_helper); 
+//! let mut input_map = InputMap::new(&input_helper);
 //!
 //! input_map.update(); // Read queued events and update the input state.
-//! input_map.is_pressed("a"); // Query the input state. 
+//! input_map.is_pressed("a"); // Query the input state.
 //! ```
 //!
 
-use std::sync::{Arc, Mutex}; 
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
 use event_feed::*;
 
@@ -87,7 +87,6 @@ pub enum InputEvent {
     KeyUp(InputName),
     // ext. ext.
 }
-
 
 /// Represents the current state of an input for the [InputMap].
 #[derive(Debug, Clone, Copy)]
@@ -122,15 +121,21 @@ impl InputHelper {
             events: Arc::from(Mutex::from(Feed::new())),
         }
     }
-    
+
     /// Create a new [InputReader].
     pub fn reader(&self) -> Arc<Reader<InputEvent>> {
-        self.events.lock().expect("failed to unlock the event feed").add_reader()
+        self.events
+            .lock()
+            .expect("failed to unlock the event feed")
+            .add_reader()
     }
-    
+
     /// Send an [InputEvent] through the input helper.
     pub fn send(&self, input: InputEvent) {
-        self.events.lock().expect("failed to unlock the event feed").send(input);
+        self.events
+            .lock()
+            .expect("failed to unlock the event feed")
+            .send(input);
     }
 }
 
@@ -153,10 +158,10 @@ impl InputMap {
             input_reader: input_helper.reader(),
         }
     }
-    
+
     /// Process all queued [InputEvent]s and update the map.
     ///
-    /// This method should be ran before inspecting the input state.  If you're 
+    /// This method should be ran before inspecting the input state.  If you're
     /// writing a framework using Input Helper, then you should probably run this before
     /// running the user's `update` code.
     pub fn update(&mut self) {
@@ -171,7 +176,7 @@ impl InputMap {
             }
         }
     }
-    
+
     /// Query the state of a button, and return `true` if the button is pressed.
     ///
     /// If the requested input is not a button, then `false` will be returned.
@@ -179,7 +184,7 @@ impl InputMap {
         match self.map.get(input) {
             Some(input) => match input {
                 InputState::Key(button) => button.is_pressed(),
-            }
+            },
             None => false,
         }
     }
