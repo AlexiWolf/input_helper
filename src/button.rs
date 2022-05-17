33a251cc5dc10d59db7fc684a134353172e49ee8
AlexiWolf <alexiwolf@pm.me>
2022-05-16 22:45:09 -0500

    Created Button trait

--- src/button.rs
+++ src/button.rs
@@ -1,3 +1,7 @@
+pub trait Button {
+    fn is_pressed(&self) -> bool;
+}
+
 /// Provides state for a button, indicating if it is currently up or down.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum ButtonState {
@@ -5,8 +9,8 @@ pub enum ButtonState {
     Down,
 }
 
-impl ButtonState {
-    pub fn is_pressed(&self) -> bool {
+impl Button for ButtonState {
+    fn is_pressed(&self) -> bool {
         match self {
             ButtonState::Up => false,
             ButtonState::Down => true,
